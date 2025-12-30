4ac959d73576a3937a3b88301cea09bff2cb4947
CodeWithCompound <mathematico.loco@gmail.com>
2025-12-30 19:53:31 +0100

    init commit

---
+++ script.py
@@ -0,0 +1,28 @@
+def calculator():
+    try:
+        num1 = float(input("first number: "))
+        operator = input("operator (+, -, *, /): ")
+        num2 = float(input("second number: "))
+
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            if num2 == 0:
+                print("ERR: my math teacher would scream: can't divide by zero")
+                return
+            result = num1 / num2
+        else:
+            print("ERR: operator must be +, -, *, /")
+            return
+
+        print(f"Result: {result}")
+
+    except ValueError:
+        print("ERR: enter valid number (no letters)")
+
+
+calculator()
